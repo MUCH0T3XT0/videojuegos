53216801277812d34b9e7f68a03709e21425a492
Horacio <A01712206@tec.mx>
2024-03-22 13:50:40 -0600

    Try to fix flake8

--- memory.py
+++ memory.py
@@ -7,57 +7,12 @@ car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-tap_count = 0  
-
-
-def square(x, y):
-    """Draw white square with black outline at (x, y)."""
-    up()
-    goto(x, y)
-    down()
-    color('black', 'white')
-    begin_fill()
-    for count in range(4):
-        forward(50)
-        left(90)
-    end_fill()
-
-
-def index(x, y):
-    """Convert (x, y) coordinates to tiles index."""
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
-
-
-def xy(count):
-    """Convert tiles count to (x, y) coordinates."""
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
-
-
-def tap(x, y):
-    """Update mark and hidden tiles based on tap."""
-    global tap_count  
-    tap_count += 1  
-    spot = index(x, y)
-    mark = state['mark']
-
-    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
-        state['mark'] = spot
-    else:
-        hide[spot] = False
-        hide[mark] = False
-        state['mark'] = None
-
-    
-    print("Número de taps:", tap_count)
-
-
-def all_tiles_uncovered():
-    """Checks if all tiles have been uncovered."""
-    return all(not tile_hidden for tile_hidden in hide)
+tap_count = 0
 
 
 def draw():
-    """Draw image and tiles."""
+    """Draws the image and tiles."""
+    global tap_count
     clear()
     goto(0, 0)
     shape(car)
@@ -80,9 +35,53 @@ def draw():
     update()
     ontimer(draw, 100)
 
-    
+
+def square(x, y):
+    """Draws a white square with a black outline at (x, y)."""
+    up()
+    goto(x, y)
+    down()
+    color('black', 'white')
+    begin_fill()
+    for count in range(4):
+        forward(50)
+        left(90)
+    end_fill()
+
+
+def index(x, y):
+    """Converts (x, y) coordinates to a tiles index."""
+    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+
+
+def xy(count):
+    """Converts tiles count to (x, y) coordinates."""
+    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+
+
+def tap(x, y):
+    """Updates mark and hidden tiles based on click."""
+    global tap_count
+    tap_count += 1
+    spot = index(x, y)
+    mark = state['mark']
+
+    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+        state['mark'] = spot
+    else:
+        hide[spot] = False
+        hide[mark] = False
+        state['mark'] = None
+
+    print("Number of taps:", tap_count)
+
     if all_tiles_uncovered():
-        print("¡Todos los cuadros se han destapado!")
+        print("All tiles have been uncovered!")
+
+
+def all_tiles_uncovered():
+    """Checks if all tiles have been uncovered."""
+    return all(not tile_hidden for tile_hidden in hide)
 
 
 shuffle(tiles)
