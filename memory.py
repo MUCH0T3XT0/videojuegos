c53c7ed174fa53363cb3657e056e87d6917cd010
Horacio <A01712206@tec.mx>
2024-03-22 10:50:07 -0600

    Modified Videogame

--- memory.py
+++ memory.py
@@ -1,18 +1,9 @@
-"""Memory, puzzle game of number pairs.
+import random
+import turtle
 
-Exercises:
-
-1. Count and print how many taps occur.
-2. Decrease the number of tiles to a 4x4 grid.
-3. Detect when all tiles are revealed.
-4. Center single-digit tile.
-5. Use letters instead of tiles.
-"""
-
-from random import *
-from turtle import *
 
 from freegames import path
+
 
 car = path('car.gif')
 tiles = list(range(32)) * 2
@@ -20,32 +11,32 @@ state = {'mark': None}
 hide = [True] * 64
 
 
-def square(x, y):
-    """Draw white square with black outline at (x, y)."""
-    up()
-    goto(x, y)
-    down()
-    color('black', 'white')
-    begin_fill()
-    for count in range(4):
-        forward(50)
-        left(90)
-    end_fill()
+def draw_square(x, y):
+    """Draws a white square with black outline at (x, y)."""
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.color('black', 'white')
+    turtle.begin_fill()
+    for _ in range(4):
+        turtle.forward(50)
+        turtle.left(90)
+    turtle.end_fill()
 
 
-def index(x, y):
-    """Convert (x, y) coordinates to tiles index."""
+def index_from_coordinates(x, y):
+    """Converts (x, y) coordinates to a tiles index."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
 
-def xy(count):
-    """Convert tiles count to (x, y) coordinates."""
+def coordinates_from_index(count):
+    """Converts tiles count to (x, y) coordinates."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 
-def tap(x, y):
-    """Update mark and hidden tiles based on tap."""
-    spot = index(x, y)
+def handle_click(x, y):
+    """Updates mark and hidden tiles based on click."""
+    spot = index_from_coordinates(x, y)
     mark = state['mark']
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
@@ -57,35 +48,35 @@ def tap(x, y):
 
 
 def draw():
-    """Draw image and tiles."""
-    clear()
-    goto(0, 0)
-    shape(car)
-    stamp()
+    """Draws the image and tiles."""
+    turtle.clear()
+    turtle.goto(0, 0)
+    turtle.shape(car)
+    turtle.stamp()
 
     for count in range(64):
         if hide[count]:
-            x, y = xy(count)
-            square(x, y)
+            x, y = coordinates_from_index(count)
+            draw_square(x, y)
 
     mark = state['mark']
 
     if mark is not None and hide[mark]:
-        x, y = xy(mark)
-        up()
-        goto(x + 2, y)
-        color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        x, y = coordinates_from_index(mark)
+        turtle.up()
+        turtle.goto(x + 2, y)
+        turtle.color('black')
+        turtle.write(tiles[mark], font=('Arial', 30, 'normal'))
 
-    update()
-    ontimer(draw, 100)
+    turtle.update()
+    turtle.ontimer(draw, 100)
 
 
-shuffle(tiles)
-setup(420, 420, 370, 0)
-addshape(car)
-hideturtle()
-tracer(False)
-onscreenclick(tap)
+random.shuffle(tiles)
+turtle.setup(420, 420, 370, 0)
+turtle.addshape(car)
+turtle.hideturtle()
+turtle.tracer(False)
+turtle.onscreenclick(handle_click)
 draw()
-done()
+turtle.done()
