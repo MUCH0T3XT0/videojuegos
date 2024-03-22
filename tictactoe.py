fe48917cc014a01c0edebf92c4ffb77917506b90
MUCH0T3XT0 <A01276139@tec.mx>
2024-03-22 13:12:38 -0600

    Change the size and color of the X and O symbols and center them

--- tictactoe.py
+++ tictactoe.py
@@ -7,34 +7,43 @@ Exercises
 3. How would you detect when someone has won?
 4. How could you create a computer player?
 """
-
-from turtle import *
-
+# Import libraries
+import turtle
 from freegames import line
 
 
+# Make the grid of the game
 def grid():
-    """Draw tic-tac-toe grid."""
+    """Set grid color"""
+    turtle.color("green")
+    """Draw the grid"""
     line(-67, 200, -67, -200)
     line(67, 200, 67, -200)
     line(-200, -67, 200, -67)
     line(-200, 67, 200, 67)
 
 
+# Draw the X player
 def drawx(x, y):
-    """Draw X player."""
-    line(x, y, x + 133, y + 133)
-    line(x, y + 133, x + 133, y)
+    """Set X color"""
+    turtle.color("yellow")
+    """Draw X figure"""
+    line(x + 33, y + 33, x + 101, y + 101)
+    line(x + 33, y + 101, x + 101, y + 33)
 
 
+# Draw the O player
 def drawo(x, y):
-    """Draw O player."""
-    up()
-    goto(x + 67, y + 5)
-    down()
-    circle(62)
+    """Set O color"""
+    turtle.color("blue")
+    """Draw O figure"""
+    turtle.up()
+    turtle.goto(x + 67, y + 27)
+    turtle.down()
+    turtle.circle(40)
 
 
+# Define the game floor
 def floor(value):
     """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
@@ -51,14 +60,17 @@ def tap(x, y):
     player = state['player']
     draw = players[player]
     draw(x, y)
-    update()
+    turtle.update()
     state['player'] = not player
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+turtle.setup(420, 420, 370, 0)
+# Set the background color
+turtle.bgcolor('black')
+# Activate the grid and turtle functions
+turtle.hideturtle()
+turtle.tracer(False)
 grid()
-update()
-onscreenclick(tap)
-done()
+turtle.update()
+turtle.onscreenclick(tap)
+turtle.done()
